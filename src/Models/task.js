1f3293102f3c971cc655bde7d5f1e82ba133f07b
Victor Mateus Magela Amato Ferreira <victormateusamato@outlook.com>
2026-05-15 00:04:13 -0300

    Initial iteration of Task

---
+++ src/Models/task.js
@@ -0,0 +1,61 @@
+class Task {
+    #id;
+    #title;
+    #description;
+    #dueDate;
+    #isDone;
+    #priority;
+
+    constructor(id, title, description, dueDate, isDone, priority) {
+        this.#id = id;
+        this.#title = title;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#isDone = isDone;
+        this.#priority = priority;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get title() {
+        return this.#title;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get dueDate() {
+        return this.#dueDate;
+    }
+
+    get isDone() {
+        return this.#isDone;
+    }
+
+    get priority() {
+        return this.#priority;
+    }
+
+    set title(title) {
+        this.#title = title;
+    }
+
+    set description(description) {
+        this.#description = description;
+    }
+
+    set dueDate(dueDate) {
+        this.#dueDate = dueDate;
+    }
+
+    set isDone(isDone) {
+        this.#isDone = isDone;
+    }
+
+    set priority(priority) {
+        this.#priority = priority;
+    }
+}
