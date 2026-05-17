2b51e86312281c151f6b9f3953b8599540aa7eaa
Victor Mateus Magela Amato Ferreira <victormateusamato@outlook.com>
2026-05-17 01:40:20 -0300

    On clicking create, the new task is added to the active project

--- src/components/modals/new-task-modal.js
+++ src/components/modals/new-task-modal.js
@@ -1,15 +1,37 @@
 import "../../styles/modals.css";
 
 import Project from "../../Models/project.js";
+import stateManager from "../../../services/state-manager.js";
+
 
 export default function loadNewTaskModal() {
+    const activeProject = stateManager.getCurrentProject();
+    
+    
     const newTaskBtn = document.getElementById('newTaskBtn');
     const newTaskDialog = document.getElementById('newTaskDialog');
     const newTaskForm = document.getElementById('newTaskForm');
     const cancelBtn = document.getElementById('cancelTaskBtn');
 
     newTaskBtn.addEventListener('click', () => {
+        if (!activeProject) {
+            alert('Please select a project first.');
+            return;
+        }
+
         newTaskDialog.showModal();
+    })
+
+    newTaskForm.addEventListener('submit', () => {
+        const formData = new FormData(newTaskForm);
+        const taskTitle = formData.get('taskTitle');
+        const taskDescription = formData.get('taskDescription');
+        const taskDueDate = formData.get('taskDueDate');
+        const taskPriority = formData.get('taskPriority');
+
+        activeProject.addTask(taskTitle, taskDescription, taskDueDate, taskPriority);
+        
+        newTaskForm.reset();
     })
 
     cancelBtn.addEventListener('click', () => {
